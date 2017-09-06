5e05f714b4e8f8babbc8929eb0ff10f399ab75be
Trent Bennett <rehab.rtb@gmail.com>
2017-09-06 13:46:34 -0600

    modified

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
 const express = require('express');
 const session = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const strategy = require(`${__dirname}/strategy.js`);
 
 const app = express();
+//MASSIVE RIGHT HERE
+//BODY PARSER RIGHT HERE
+
+//MUST BE DONE IN THIS ORDER!!
+app.use( session({
+  secret: 'sup dude',
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
+
+passport.serializeUser(function(user, done) {
+  //GETS USER FROM AUTH0 PROFILE
+  var userInfo = {
+    id: user.id,
+    display: user.displayName,
+    nickname: user.nickname,
+    email: user.email
+  }
+  done(null, userInfo)
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user); //PUTS ON req.user
+});
+
+app.get('/login', passport.authenticate('auth0', {
+  successRedirect: '/me',
+  failureRedirect: '/',
+  failureFlash: true
+}));
+
+app.get('/me', function(req, res, next){
+  res.json(req.user)
+})
+
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
